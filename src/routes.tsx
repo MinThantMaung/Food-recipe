d92874dba49fc9a8543231e7a22e79f285933b31
Min Thant Mg <minthant180@gmail.com>
2026-09-07 02:06:20 +0900

    feat: add navigation for whole web

---
+++ src/routes.tsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import RootLayout from "./pages/RootLayout";
+import Home from "./pages/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: "Error",
+    children: [
+      { index: true, element: <Home />},
+    ],
+  }
+]);
