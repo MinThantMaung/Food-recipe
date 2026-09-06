4c551c2fcde734d5e1535c1ce496afa9769d6087
Min Thant Mg <minthant180@gmail.com>
2026-09-07 02:06:57 +0900

    feat: rootlayout for main web pages

---
+++ src/pages/RootLayout.tsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+// import Header from "../components/layouts/Header"
+// import Footer from "../components/layouts/Footer";
+
+function RootLayout() {
+    return (
+        <div className="flex flex-col min-h-screen overflow-hidden">
+            {/* <Header /> */}
+            <main className="flex-1 mt-16">
+                <Outlet />
+            </main>
+            {/* <Footer /> */}
+        </div>
+
+    );
+}
+
+export default RootLayout;
