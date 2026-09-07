24438b922899dc740a88338e1a781e41630bf6e6
Min Thant Maung <minthant180@gmail.com>
2026-09-07 17:23:27 +0900

    feat: shadcn ui setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from "react-router-dom";
+
+import { router } from "./routes";
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div>Hello</div>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
