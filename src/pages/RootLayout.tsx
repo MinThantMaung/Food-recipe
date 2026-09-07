3bb8915da7056e56db53dae2a7a2d2348ec3fca8
Min Thant Maung <minthant180@gmail.com>
2026-09-07 17:27:09 +0900

    feat: header and footer setup

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,17 +1,16 @@
 import { Outlet } from "react-router-dom";
-// import Header from "../components/layouts/Header"
-// import Footer from "../components/layouts/Footer";
+import Header from "../components/layouts/Header"
+import Footer from "../components/layouts/Footer";
 
 function RootLayout() {
     return (
         <div className="flex flex-col min-h-screen overflow-hidden">
-            {/* <Header /> */}
+            <Header />
             <main className="flex-1 mt-16">
                 <Outlet />
             </main>
-            {/* <Footer /> */}
+            <Footer />
         </div>
-
     );
 }
 
