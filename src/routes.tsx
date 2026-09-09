1dcfb51cc779cacbdf835b2d74e52276f9416eaa
Min Thant Maung <minthant180@gmail.com>
2026-09-09 16:16:54 +0900

    feat:implement action,loader and navigation route

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import RootLayout from "./pages/RootLayout";
+import AuthRootLayout from "./pages/AuthRootLayout.tsx";
 import Home from "./pages/Home";
 import LoginPage from "./pages/auth/Login.tsx"
 import RegisterPage from "./pages/auth/Register.tsx"
 import VerifyOtpPage from "./pages/auth/VerifyOtp.tsx";
 import ConfirmPasswordPage from "./pages/auth/ConfirmPassword.tsx";
+import { confirmPasswordAction, registerAction, verifyOtpAction } from "./router/action/index.ts";
 
 export const router = createBrowserRouter([
   {
@@ -20,12 +22,24 @@ export const router = createBrowserRouter([
     path: "/login", element: <LoginPage />
   },
   {
-    path: "/register", element: <RegisterPage />
+    path: "/register", 
+    element: <AuthRootLayout />,
+    children: [
+      {
+        index: true,
+        element: <RegisterPage />,
+        action: registerAction
+      },
+      {
+        path: "verify-otp",
+        element: <VerifyOtpPage />,
+        action: verifyOtpAction
+      },
+      {
+        path: "confirm-password",
+        element: <ConfirmPasswordPage />,
+        action: confirmPasswordAction
+      }
+    ]
   },
-  {
-    path: "/verify-otp", element: <VerifyOtpPage />
-  },
-  {
-    path: "/confirm-password", element: <ConfirmPasswordPage />
-  }
 ]);
