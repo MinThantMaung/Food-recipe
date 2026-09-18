d9394515367cb5ddf7c4e85be79fc32ba8129544
Min Thant Maung <minthant180@gmail.com>
2026-09-18 17:30:21 +0900

    feat:update router loader

--- src/router/loader/index.ts
+++ src/router/loader/index.ts
@@ -1,5 +1,6 @@
 import { authApi } from "@/api";
 import useAuthStore, { Status } from "@/stores/authStore";
+import axios from "axios";
 import { redirect } from "react-router";
 
 export const loginLoader = async () => {
@@ -7,7 +8,13 @@ export const loginLoader = async () => {
     await authApi.get("auth-check");
     return redirect("/");
   } catch (error) {
-    console.log("Loader error:", error);
+    if (
+      axios.isAxiosError(error) &&
+      error.response?.status === 401
+    ) {
+      return null;
+    }
+    return null;
   }
 };
 
@@ -29,4 +36,20 @@ export const confirmLoader = async () => {
   }
 
   return null;
+};
+
+export const homeLoader = async () => {
+  try {
+    const response = await authApi.get("auth-check");
+
+    // Authenticated: allow Home to render
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response?.status === 401) {
+      // Not authenticated: redirect to Login
+      return redirect("/login");
+    }
+
+    throw error;
+  }
 };
