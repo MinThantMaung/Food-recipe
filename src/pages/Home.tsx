1022ac3b9bc08ea67e333658831c8b8d32dc3d04
Min Thant Maung <minthant180@gmail.com>
2026-09-18 17:29:48 +0900

    feat:implement temp logout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Form } from "react-router-dom";
+
 function Home() {
   return (
     <div className="container mx-auto">
-      Hello
+      <Form action="/logout" method="post">
+        <Button type="submit" variant="outline">
+          Logout
+        </Button>
+      </Form>
     </div>
   );
 }
